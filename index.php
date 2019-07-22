2d1b87f8e0dc56e3ee5a364a4dc884d37b58c379
renanhabreu <renanhabreu@gmail.com>
2019-07-22 14:52:20 -0300

    commit framework php7

--- index.php
+++ index.php
@@ -4,23 +4,14 @@ include dirname(__FILE__).'/core/autoload.php';
 
 use core\Router;
 use core\Request;
-use filter\Login;
-use filter\Logout;
-use controller\Teste;
 
 $router = new Router(new Request());
 $router->get('/', function($req) {
-    $ctr = new Teste(
-        $req, 
-        array(new Login()),
-        array(new Logout())
-    );
-    $ctr->index();   
+  //seu código   
 });
 
-$router->post('/contar', function($req) {
-    $ctr = new Teste($req);
-    $ctr->contar();   
+$router->post('/', function($req) {
+  //seu código 
 });
 
 
